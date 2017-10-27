d857d80d3fd37e0aa25f12a300c51c6346297a72
Material <So0utt@gmail.com>
2017-10-27 22:25:20 +0400

    added content, validation, names refactoring

--- scripts/tasks3.js
+++ scripts/tasks3.js
@@ -1,54 +1,48 @@
-function tag(tagName, attrs) {
-    let name = tagName.toLowerCase();
-    let jsonObj = attrs;
-    let tagPatternValidate = /[0-9-_ ]+/;
-    let attrPatternValidate = /^[a-z][a-z_\-\d]+[^_\-]$/;
-    let singleTag = /area|base|basefont|bgsound|br|col|command|embed|hr|img|isindex|input|keygen|link|meta|param|source|track|wbr/;
-    let result = "<" + name;
-    validateName(tagPatternValidate);
-    validateJson(attrPatternValidate, jsonObj);
-
-    let i = 0;
-    for (name in jsonObj) {
-        let temp = Utils.formatted(" {"+ i +"}" + "=" +"\""+ "{"+i + 1+"}"+"\"", name, jsonObj[name]);
-        result += temp;
-        //console.log(temp, "temp");
-        i++;
-    }
-
-    switch (singleTag.test(tagName)) {
-        case true:
-            result += "/>";
-            break;
-        case false:
-            result += "></"+tagName+">";
-    }
-
-    return result;
-}
-
-let obj = {
+let data = {
     name: "Evgeniy",
     lastname: "Borzunov",
     age: 23,
     developer: true
 };
 
-console.log(tag("div", obj));
-console.log(tag("br", obj));
+function tag(tagName, attributes, content) {
+    let name = tagName.toLowerCase();
+    let obj = attributes;
+    let tagValidatePattern = /[0-9-_ ]+/;
+    let objValidatePattern = /^[a-z][a-z_\-\d]+[^_\-]$/;
+    let singleTagPattern = /area|base|basefont|bgsound|br|col|command|embed|hr|img|isindex|input|keygen|link|meta|param|source|track|wbr/;
+    let result = "<" + name;
 
-function validateName(validateTag) {
+    validateTagName(name, tagValidatePattern);
+    validateAttributes(obj, objValidatePattern);
 
-    if (validateTag.exec(name)) {
-        throw Error("Tag name cannot accept numbers, spaces and etc.", tagName);
+    let i = 0;
+    for (let key in obj) {
+        let temp = Utils.formatted(" {"+ i +"}" + "=" +"\""+ "{"+i + 1+"}"+"\"", key, obj[key]);
+        result += temp;
+        i++;
+    }
+    singleTagPattern.test(name) ? result += "/>" : result +=">" + content +"</"+name+">";
+
+
+    return result;
+}
+
+function validateTagName(tagName, pattern, error = "Not valid") {
+    if (pattern.test(tagName)) {
+        throw new Error(error +": "+ tagName);
     }
 }
 
-function validateJson(pattern, object) {
-
-    for (let key in object) {
-        if(!pattern.test(key)) {
-            alert("Не валидное имя атрибута: "+ key);
+function validateAttributes(attributes, pattern, error = "Not valid") {
+    for (let key in attributes) {
+        if (!pattern.test(key)) {
+            throw new Error(error+": "+key);
         }
+
     }
 }
+console.log(tag("div", data, "просто какой то контент"));
+console.log(tag("br", data, "он оказался здесь"));
+console.log(tag("header", data, "div"));
+
